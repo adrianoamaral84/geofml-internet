113f23d264c96030c39f10dceba62aac89ec3340
adrianoamaral84 <adriano.amaral84@gmail.com>
2026-09-11 20:48:53 -0300

    Move check-in e check-out para rotas protegidas

--- routes/security_hardening.php
+++ routes/security_hardening.php
@@ -12,10 +12,28 @@ Route::middleware(['auth', 'role:hospede'])
     });
 
 Route::middleware(['auth', 'role:hospede'])
-    ->delete('/hospede/cancelar/hospedagem/{id}', 'Security\SecureHospedeActionsController@cancelarHospedagem')
+    ->delete('/hospede/cancelar/hospedagem/{id}', 'Security\SecureHospedeActionsController@cancelarReserva')
     ->name('cancelar.hospedagem');
 
 Route::middleware(['auth', 'role:hospede'])
     ->get('/hospede/cancelar/hospedagem/{id}', function () {
+        abort(405, 'Método não permitido.');
+    });
+
+Route::middleware(['auth', 'role:atendente|administrador_geral|auxiliar_administrador_geral'])
+    ->delete('/atendente/hospedagem/{id}/checkin', 'Security\SecureHospedeActionsController@checkin')
+    ->name('hospede.checkin');
+
+Route::middleware(['auth', 'role:atendente|administrador_geral|auxiliar_administrador_geral'])
+    ->delete('/atendente/hospedagem/{id}/checkout', 'Security\SecureHospedeActionsController@checkout')
+    ->name('hospede.checkout');
+
+Route::middleware(['auth'])
+    ->get('/hospede/checkin/{id}', function () {
+        abort(405, 'Método não permitido.');
+    });
+
+Route::middleware(['auth'])
+    ->get('/hospede/checkout/{id}/{hospede}', function () {
         abort(405, 'Método não permitido.');
     });
