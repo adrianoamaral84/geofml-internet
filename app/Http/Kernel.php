e6e1458bf9914da31c90f5493fb491ce04ccbf1e
adrianoamaral84 <adriano.amaral84@gmail.com>
2026-09-11 20:12:23 -0300

    Ativa alteração segura de senha no middleware web

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\BlockLegacyPasswordChange::class,
+            \App\Http\Middleware\HandleSecurePasswordChange::class,
             \App\Http\Middleware\FinalizePasswordSecurityMessages::class,
         ],
 
