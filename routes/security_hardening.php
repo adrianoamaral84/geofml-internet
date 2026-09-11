78c02ddce7173a8cf0a14db4807d0c41e7f7ecf4
adrianoamaral84 <adriano.amaral84@gmail.com>
2026-09-11 20:37:24 -0300

    Secure reservation cancellation route

--- routes/security_hardening.php
+++ routes/security_hardening.php
@@ -10,3 +10,12 @@ Route::middleware(['auth', 'role:hospede'])
     ->get('/hospede/delete/meupedido/{id}', function () {
         abort(405, 'Método não permitido.');
     });
+
+Route::middleware(['auth', 'role:hospede'])
+    ->delete('/hospede/cancelar/hospedagem/{id}', 'Security\SecureHospedeActionsController@cancelarHospedagem')
+    ->name('cancelar.hospedagem');
+
+Route::middleware(['auth', 'role:hospede'])
+    ->get('/hospede/cancelar/hospedagem/{id}', function () {
+        abort(405, 'Método não permitido.');
+    });
