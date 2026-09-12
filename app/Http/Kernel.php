97192c1f39bb8ad47c8a785ebda7f4542a978007
adrianoamaral84 <adriano.amaral84@gmail.com>
2026-09-11 21:04:33 -0300

    security: enforce ownership on hospede resources

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,6 +38,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\BlockUnsafeGetMutations::class,
             \App\Http\Middleware\EnforceRoleByArea::class,
+            \App\Http\Middleware\EnforceHospedeOwnership::class,
             \App\Http\Middleware\BlockLegacyPasswordChange::class,
             \App\Http\Middleware\HandleSecurePasswordChange::class,
             \App\Http\Middleware\HardenLegacyMutationForms::class,
