700423e174e22bade4e436b3f6cf0839a3a5ba3b
adrianoamaral84 <adriano.amaral84@gmail.com>
2026-09-12 00:50:39 -0300

    test: cover strong password policy

---
+++ tests/Feature/StrongPasswordTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Rules\StrongPassword;
+use Illuminate\Http\Request;
+use Tests\TestCase;
+
+class StrongPasswordTest extends TestCase
+{
+    public function test_accepts_strong_password()
+    {
+        $this->app->instance('request', Request::create('/', 'POST'));
+        config(['password_security.blocklist' => []]);
+
+        $rule = new StrongPassword();
+
+        $this->assertTrue($rule->passes('password', 'SenhaForte#2026'));
+    }
+
+    public function test_rejects_password_without_required_character_classes()
+    {
+        $this->app->instance('request', Request::create('/', 'POST'));
+        config(['password_security.blocklist' => []]);
+
+        $this->assertFalse((new StrongPassword())->passes('password', 'senhaforte#2026'));
+        $this->assertFalse((new StrongPassword())->passes('password', 'SENHAFORTE#2026'));
+        $this->assertFalse((new StrongPassword())->passes('password', 'SenhaForte####'));
+        $this->assertFalse((new StrongPassword())->passes('password', 'SenhaForte2026'));
+    }
+
+    public function test_rejects_password_outside_length_limits()
+    {
+        $this->app->instance('request', Request::create('/', 'POST'));
+        config(['password_security.blocklist' => []]);
+
+        $this->assertFalse((new StrongPassword())->passes('password', 'Aa1#abc'));
+        $this->assertFalse((new StrongPassword())->passes('password', 'Aa1#' . str_repeat('x', 61)));
+    }
+
+    public function test_rejects_password_containing_cpf()
+    {
+        $request = Request::create('/', 'POST', [
+            'cpf' => '123.456.789-09',
+        ]);
+        $this->app->instance('request', $request);
+        config(['password_security.blocklist' => []]);
+
+        $rule = new StrongPassword();
+
+        $this->assertFalse($rule->passes('password', 'Ab#12345678909X'));
+    }
+
+    public function test_rejects_password_containing_significant_email_part()
+    {
+        $request = Request::create('/', 'POST', [
+            'email' => 'fulano@example.com',
+        ]);
+        $this->app->instance('request', $request);
+        config(['password_security.blocklist' => []]);
+
+        $rule = new StrongPassword();
+
+        $this->assertFalse($rule->passes('password', 'Fulano#2026A'));
+    }
+}
