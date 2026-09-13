3180bd2cabd65b8c9327a80eda0c4cdaa95ad2fc
adrianoamaral84 <adriano.amaral84@gmail.com>
2026-09-13 14:09:38 -0300

    refactor: direciona confirmação do pedido para controller seguro

--- routes/internet.php
+++ routes/internet.php
@@ -62,7 +62,7 @@ Route::middleware(['auth', 'role:hospede'])->prefix('hospede')->group(function (
     Route::get('/', 'Security\\SecureHospedeController@index')->name('hospede.index');
 
     Route::get('/pedido', 'Security\\SecurePedidoController@create')->name('hospede.solicitarinscricao');
-    Route::post('/pedido/confirmar', 'Pedidos\\PedidosController@confimrarPedido')->name('hospede.confirmar');
+    Route::post('/pedido/confirmar', 'Security\\SecurePedidoController@confirmar')->name('hospede.confirmar');
     Route::post('/pedido/store', 'Pedidos\\PedidosController@store')->name('hospede.store');
 
     Route::get('/pedido/edita/{id}', 'EditarPedido\\EditarPedidoController@index')->name('hospede.solicitarinscricao.edit');
