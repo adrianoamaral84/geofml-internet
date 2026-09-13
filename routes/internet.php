c347f1c70d4e6e81ae84eb598fed61ed4e324845
adrianoamaral84 <adriano.amaral84@gmail.com>
2026-09-13 15:22:07 -0300

    refactor: usa confirmação segura na edição do pedido

--- routes/internet.php
+++ routes/internet.php
@@ -66,7 +66,7 @@ Route::middleware(['auth', 'role:hospede'])->prefix('hospede')->group(function (
     Route::post('/pedido/store', 'Security\\SecurePedidoStoreController@store')->name('hospede.store');
 
     Route::get('/pedido/edita/{id}', 'Security\\SecureEditarPedidoController@index')->name('hospede.solicitarinscricao.edit');
-    Route::post('/pedido/edita/confirmar', 'EditarPedido\\EditarPedidoController@confirmaEdicao')->name('hospede.edita.confirmar');
+    Route::post('/pedido/edita/confirmar', 'Security\\SecureEditarPedidoConfirmController@confirmar')->name('hospede.edita.confirmar');
     Route::post('/pedido/edita/store', 'EditarPedido\\EditarPedidoController@gravaEdicao')->name('hospede.storeEdit.edita');
 
     Route::get('/meuspedidos', 'Security\\SecureHospedeController@meuspedidos')->name('hospede.meuspedidos');
